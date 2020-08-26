1553e04133ed4a493806a851ceaff4f96e665253
Ellian Abe <ellian_abe@outlook.com>
2020-08-25 22:25:50 -0300

    Aula 2 - Projeto Restaurante

---
+++ Aula_2_25_08_2020/Aula02/app/src/main/java/com/uniso/lpdm/aula02/Aula02Activity.java
@@ -0,0 +1,14 @@
+package com.uniso.lpdm.aula02;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Aula02Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aula02_main);
+    }
+}
