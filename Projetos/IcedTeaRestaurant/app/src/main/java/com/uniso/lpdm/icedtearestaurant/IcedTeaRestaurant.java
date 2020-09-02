d10fa08c009da93e640ae561caca20a5f3716272
Ellian Abe <ellian_abe@outlook.com>
2020-09-01 22:46:37 -0300

    Exercicios Aula 03 2020-09-01

---
+++ Projetos/IcedTeaRestaurant/app/src/main/java/com/uniso/lpdm/icedtearestaurant/IcedTeaRestaurant.java
@@ -0,0 +1,32 @@
+//Ellian Yemon Abe -  Ciência da Computação - 00098381
+
+package com.uniso.lpdm.icedtearestaurant;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class IcedTeaRestaurant extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_iced_tea_restaurant);
+    }
+
+    // função disparada no click do botão de confirmação do pedido - por enquanto muda o valor do text view para "Registrando pedido"
+    public void onClickOrder(View view){
+        TextView textArea01 = (TextView) findViewById(R.id.textArea01);
+
+        textArea01.setText("Registrando pedido");
+    }
+
+    // função disparada no click do botão do total de horas - por enquanto muda o valor do text view para "Total de horas"
+    public void onClickGetTotalHours(View view) {
+        TextView textArea01 = (TextView) findViewById(R.id.textArea01);
+
+        textArea01.setText("Total de horas");
+    }
+}
